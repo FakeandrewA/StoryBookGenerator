adf3914a97b7cc0a6935263616193fc4e895c75e
Vanshika_ <Vanshikaagarwal7010@gmail.com>
2025-07-22 21:14:54 +0530

    commit message (you can change this)

--- storybookgenerator/urls.py
+++ storybookgenerator/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import homepage, book_view
+from . import views
 
 app_name = "storybookgenerator"
 
 urlpatterns = [
-    path("homepage/", homepage, name="homepage"),
-    path("book/", book_view, name="book_view"),
+    path("homepage/", views.homepage, name="homepage"),
+    path("book/", views.book_view, name="book"),
+    path("login/",views.login_view,name="login"),
+     path('register/',views.register_view, name='register'),
+   
 ]
--- storybookgenerator/views.py
+++ storybookgenerator/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 def homepage(request):
@@ -6,3 +8,19 @@ def homepage(request):
 
 def book_view(request):
     return render(request, "storybookgenerator/book_view.html")
+
+def login_view(request):
+   
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('storybookgenerator:homepage')
+        else:
+            messages.error(request, "Invalid username or password.")
+    
+    return render(request, 'login.html')
+
