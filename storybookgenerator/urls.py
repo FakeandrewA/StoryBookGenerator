b4b788ad8163724ca3d3f649c9274c8110e11f3e
[Andrew A] <[andrewgtc2005@gmail.com]>
2025-07-23 12:10:39 +0530

    Changed UI - to adopt to the main theme
    added authentication
    
    added logout

--- storybookgenerator/urls.py
+++ storybookgenerator/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import homepage, book_view, login_view,register_view
+from .views import homepage, book_view, login_view, register_view, logout_view
 
 app_name = "storybookgenerator"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("book/", book_view, name="book_view"),
     path("login/", login_view, name="login"),
     path("register/", register_view, name="register"),
+    path("logout/", logout_view, name="logout"),
 ]
